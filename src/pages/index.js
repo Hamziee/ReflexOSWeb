ea5e3a99ed2b3c0df60ea4aef9d54f3ceed1a214
Hamziee <91210085+Hamziee@users.noreply.github.com>
2023-05-06 17:58:56 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`ReflexOS`}
+      description="ReflexOS is a transparent and streamlined modification of Windows. Designed to maximize your system's performance and optimize latency. Without removing Windows Update or Defender!">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
